efc6bd0f4497574f8269c29ea037d228a3d6fccb
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2026-08-18 06:54:23 -0500

    Added database testing with drama database

---
+++ tests/db/seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO dramas
+    (slug, title, status, rating, image, url)
+VALUES
+    ('756307-manager-kim', 'Agent Kim Reactivated', 'Watching', NULL, '', 'https://mydramalist.com/756307-manager-kim'),
+    ('769787-dong-feng-xin', 'Blossoms of Power', 'Watching', NULL, '', 'https://mydramalist.com/769787-dong-feng-xin'),
+    ('784484-i-give-it-to-you', 'Dream to You', 'Watching', NULL, '', 'https://mydramalist.com/784484-i-give-it-to-you'),
+    ('790870-que-gu', 'Key to the Phoenix Heart', 'Watching', 10.0, '', 'https://mydramalist.com/790870-que-gu'),
+    ('798802-chilling-romance', 'Spooky in Love', 'Watching', NULL, '', 'https://mydramalist.com/798802-chilling-romance'),
+    ('746387-agent-from-above', 'Agent from Above', 'Completed', 8.0, '', 'https://mydramalist.com/746387-agent-from-above'),
+    ('779398-colony', 'Colony', 'Completed', 7.5, '', 'https://mydramalist.com/779398-colony'),
+    ('49241-falling-into-your-smile', 'Falling into Your Smile', 'Completed', 10.0, '', 'https://mydramalist.com/49241-falling-into-your-smile'),
+    ('696033-mei-fu-ren-chong-fu-ri-chang', 'Moonlit Reunion', 'Completed', 10.0, '', 'https://mydramalist.com/696033-mei-fu-ren-chong-fu-ri-chang'),
+    ('783100-a-shop-for-killers-season-2', 'A Shop for Killers Season 2', 'Plan to Watch', NULL, '', 'https://mydramalist.com/783100-a-shop-for-killers-season-2'),
+    ('808312-geurin-raiteu', 'Geurinraiteu', 'Plan to Watch', NULL, '', 'https://mydramalist.com/808312-geurin-raiteu'),
+    ('762921-unknown-seoul', 'Our Unwritten Seoul', 'On-hold', NULL, '', 'https://mydramalist.com/762921-unknown-seoul'),
+    ('686405-ask-the-stars', 'When the Stars Gossip', 'Dropped', NULL, '', 'https://mydramalist.com/686405-ask-the-stars');
